20fd3ee258acb71f2bf30ecb93ba8e772817f23a
doannx <doannx@gmail.com>
2018-07-04 22:31:01 +0700

    Add init version of getColorByScore()

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,15 +10,33 @@
             Movie name <span>Movie rating</span>
         </li>
     </ul>
+    {{info}}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   data () {
     return {
+      info: null,
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  },
+  methods: {
+    getColorByScore (score) {
+      switch (score) {
+        case 8:
+          return '#2196f3'
+        default:
+          break
+      }
     }
   }
 }
