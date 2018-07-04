402379d43e3b92a2f2541853dd981114e8404212
doannx <doannx@gmail.com>
2018-07-04 22:01:57 +0700

    Init main page.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import '@/assets/css/ress.css'
+import '@/assets/css/style.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
